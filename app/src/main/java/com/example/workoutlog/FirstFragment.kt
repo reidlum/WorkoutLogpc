8dcf97017e0a8e5960d47e8cdd613ffb84dd96d9
reidlum <reidlum@hawaii.edu>
2022-08-04 01:12:32 -1000

    little things

--- app/src/main/java/com/example/workoutlog/FirstFragment.kt
+++ app/src/main/java/com/example/workoutlog/FirstFragment.kt
@@ -6,7 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.workoutlog.adapter.ItemAdapter
+import com.example.workoutlog.data.Datasource
 import com.example.workoutlog.databinding.FragmentFirstBinding
+
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -14,6 +20,7 @@ import com.example.workoutlog.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+    private lateinit var recyclerView: RecyclerView
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,6 +34,8 @@ class FirstFragment : Fragment() {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
+        //@TODO FIGURE OUT RECYCLER VIEW FOR WORKOUTS
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,6 +43,9 @@ class FirstFragment : Fragment() {
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_addWorkoutFragment)
         }
     }
 
