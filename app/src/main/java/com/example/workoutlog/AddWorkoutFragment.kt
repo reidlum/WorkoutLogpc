91d10274199bf0d3e7e60e129fc35b8420bf6b8c
reidlum <reidlum@hawaii.edu>
2022-08-04 22:28:00 -1000

    Added nav to save on addworkout

--- app/src/main/java/com/example/workoutlog/AddWorkoutFragment.kt
+++ app/src/main/java/com/example/workoutlog/AddWorkoutFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.workoutlog.databinding.FragmentAddWorkoutBinding
+import com.example.workoutlog.databinding.FragmentFirstBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +23,8 @@ class AddWorkoutFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentAddWorkoutBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +39,16 @@ class AddWorkoutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_workout, container, false)
+        //return inflater.inflate(R.layout.fragment_add_workout, container, false)
+        _binding = FragmentAddWorkoutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.saveBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_addWorkoutFragment_to_FirstFragment2)
+        }
     }
 
     companion object {
@@ -55,5 +69,9 @@ class AddWorkoutFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
