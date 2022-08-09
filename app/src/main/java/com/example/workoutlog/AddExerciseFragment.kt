9b3a955fd5a718fdce7d2397e5d7b1f3f8111cdf
reidlum <reidlum@hawaii.edu>
2022-08-09 01:00:49 -1000

    Exercise nav and changed reps/lbs to number input

--- app/src/main/java/com/example/workoutlog/AddExerciseFragment.kt
+++ app/src/main/java/com/example/workoutlog/AddExerciseFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.workoutlog.databinding.FragmentAddExerciseBinding
+import com.example.workoutlog.databinding.FragmentAddWorkoutBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +23,8 @@ class AddExerciseFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentAddExerciseBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +39,16 @@ class AddExerciseFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_exercise, container, false)
+        //return inflater.inflate(R.layout.fragment_add_exercise, container, false)
+        _binding = FragmentAddExerciseBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.saveBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_addExerciseFragment_to_SecondFragment)
+        }
     }
 
     companion object {
@@ -55,5 +69,10 @@ class AddExerciseFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
