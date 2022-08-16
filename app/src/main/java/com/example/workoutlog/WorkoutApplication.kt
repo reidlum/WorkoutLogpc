bc3e69269cd3cfc887ace634ac78112ae09c1122
reidlum <reidlum@hawaii.edu>
2022-08-16 13:09:03 -1000

    Exercise database

--- app/src/main/java/com/example/workoutlog/WorkoutApplication.kt
+++ app/src/main/java/com/example/workoutlog/WorkoutApplication.kt
@@ -1,8 +1,11 @@
 package com.example.workoutlog
 
 import android.app.Application
+import com.example.workoutlog.data.Exercise
+import com.example.workoutlog.data.ExerciseRoomDatabase
 import com.example.workoutlog.data.WorkoutRoomDatabase
 
 class WorkoutApplication : Application(){
     val database: WorkoutRoomDatabase by lazy { WorkoutRoomDatabase.getDatabase(this) }
+    val edatabase: ExerciseRoomDatabase by lazy { ExerciseRoomDatabase.getDatabase(this) }
 }
