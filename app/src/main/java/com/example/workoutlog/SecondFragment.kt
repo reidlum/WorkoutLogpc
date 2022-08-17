85b7afafb6d25675e7afdd066bf7937f034e2670
reidlum <reidlum@hawaii.edu>
2022-08-16 23:16:39 -1000

    bind workout

--- app/src/main/java/com/example/workoutlog/SecondFragment.kt
+++ app/src/main/java/com/example/workoutlog/SecondFragment.kt
@@ -113,10 +113,10 @@ class SecondFragment : Fragment() {
             //val action = SecondFragmentDirections.actionSecondFragmentToAddExerciseFragment(workoutName = workoutName, workoutId = id)
             //findNavController().navigate(action)
         //}
-        //viewModel.retrieveWorkout(id).observe(this.viewLifecycleOwner) { selectedItem ->
-        //    workout = selectedItem
-        //    bind(workout)
-        //}
+        viewModel.retrieveWorkout(id).observe(this.viewLifecycleOwner) { selectedItem ->
+            workout = selectedItem
+            bind(workout)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
